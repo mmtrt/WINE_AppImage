ca0f2a810e2b20f0cad8e8f30645cf4ed4e40310
Taqi Raza <mttalpur@gmail.com>
2020-09-26 10:47:34 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ sudo apt-get update ;
 sudo apt-get -y install python3 python3-setuptools python3-pip wget patchelf fakeroot gnupg2 libglib2.0-bin file desktop-file-utils libgdk-pixbuf2.0-dev librsvg2-dev zsync
 
 
-mkdir -p ./{AppDir,workdir} ; cp ./org.winehq.wine.desktopAppDir ;
+mkdir -p ./{AppDir,workdir} ; cp org.winehq.wine.desktop AppDir ;
 sudo mkdir -p /usr/share/icons/hicolor/scalable/ ; sudo cp wine.svg /usr/share/icons/hicolor/scalable/ ;
 cd workdir ; sudo pip3 install appimage-builder ;
 wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage && chmod +x appimagetool-x86_64.AppImage ;
