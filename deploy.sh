f79139954cbadd42b7c8b181d3c2434116d579a0
Taqi Raza <mttalpur@gmail.com>
2020-09-26 18:42:37 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ sudo apt-get -y install python3 python3-setuptools python3-pip wget patchelf fak
 sudo mkdir -p /usr/share/icons/hicolor/scalable/ ; sudo cp wine.svg /usr/share/icons/hicolor/scalable/ ;
 sudo pip3 install appimage-builder ;
 wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage && chmod +x appimagetool-x86_64.AppImage ;
-./appimagetool-x86_64.AppImage --appimage-extract && mv squashfs-root appimage-tool.AppDir ;
-sudo ln -s ./appimage-tool.AppDir/AppRun /usr/local/bin/appimagetool && sudo chmod +x /usr/local/bin/appimagetool && rm appimagetool-x86_64.AppImage  && ls -al ;
+./appimagetool-x86_64.AppImage --appimage-extract && mv squashfs-root appimage-tool.AppDir && chmod +x ./appimage-tool.AppDir/AppRun  ;
+sudo ln -s ./appimage-tool.AppDir/AppRun /usr/local/bin/appimagetool && rm appimagetool-x86_64.AppImage  && ls -al ;
