aa2ab8ddc4746f6a790e3e94a102bb4673905f63
Taqi Raza <mttalpur@gmail.com>
2020-09-26 10:20:48 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,18 +9,4 @@ wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appima
 ./appimagetool-x86_64.AppImage --appimage-extract && mv squashfs-root appimage-tool.AppDir ;
 sudo ln -s appimage-tool.AppDir/AppRun /usr/bin/appimagetool && rm appimagetool-x86_64.AppImage ;
 sudo mkdir -p /usr/share/icons/hicolor/256x256/apps/ ;
-cat > org.winehq.wine.desktop <<'EOF'
-[Desktop Entry]
-X-AppImage-Arch=x86_64
-X-AppImage-Version=staging
-X-AppImage-Name=wine
-Name=wine
-Exec=opt/wine-staging/bin/wine64
-Icon=wine
-Type=Application
-Terminal=false
-Categories=Utility;
-Comment=
-EOF
-sudo mv org.winehq.wine.desktop /usr/share/applications/ ;
 sudo wget https://github.com/mmtrt/Wine_Appimage_old/raw/master/resource/Wine.png -O /usr/share/icons/hicolor/256x256/apps/wine.png
