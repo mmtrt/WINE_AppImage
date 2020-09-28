19406435a02e4930968a1d11d12e350c136b67b9
Taqi Raza <mttalpur@gmail.com>
2020-09-28 18:06:53 +0500

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update ;
-sudo apt-get -y install python3 python3-setuptools python3-pip wget patchelf fakeroot gnupg2 libglib2.0-bin file desktop-file-utils libgdk-pixbuf2.0-dev librsvg2-dev zsync
-
-sudo mkdir -p /usr/share/icons/hicolor/scalable/ ; sudo cp wine.svg /usr/share/icons/hicolor/scalable/ ;
-sudo pip3 install appimage-builder ;
-wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage && chmod +x appimagetool-x86_64.AppImage ;
-mv appimagetool-x86_64.AppImage appimagetool && cp appimagetool /home/travis/bin && ls -al && ls -al /home/travis/bin ;
