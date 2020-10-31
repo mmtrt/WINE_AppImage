16319c27a24cd1dcbb5d9de52234866355f9b103
Taqi Raza <mttalpur@gmail.com>
2020-10-31 09:07:21 +0500

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -33,16 +33,139 @@ pacman -Syu --noconfirm
 #Add "base-devel multilib-devel" for compile in the list:
 pacman -S --noconfirm wget base-devel multilib-devel pacman-contrib git tar grep sed zstd xz bzip2 patchelf python-pip
 
-wineworkdir=(wineversion)
-cd $wineworkdir
-
-# Add a dependency library, such as freetype font library
-dependencys=$(pactree -s -u wine |grep lib32 | xargs)
-
 mkdir cache
 
-pacman -Scc --noconfirm
-pacman -Syw --noconfirm --cachedir cache lib32-libxmu lib32-libxxf86vm lib32-mesa lib32-mesa-libgl lib32-ocl-icd lib32-vulkan-icd-loader lib32-libdrm lib32-libva lib32-vulkan-intel lib32-vulkan-radeon $dependencys
+# wine deps
+pacman -Syw --noconfirm --cachedir cache \
+fontconfig \
+lib32-fontconfig \
+lcms2 \
+lib32-lcms2 \
+libxml2 \
+lib32-libxml2 \
+libxcursor \
+lib32-libxcursor \
+libxrandr \
+lib32-libxrandr \
+libxdamage \
+lib32-libxdamage \
+libxi \
+lib32-libxi \
+gettext \
+lib32-gettext \
+freetype2 \
+lib32-freetype2 \
+glu \
+lib32-glu \
+libsm \
+lib32-libsm \
+gcc-libs \
+lib32-gcc-libs \
+libpcap \
+lib32-libpcap \
+faudio \
+lib32-faudio \
+desktop-file-utils \
+giflib \
+lib32-giflib \
+libpng \
+lib32-libpng \
+libldap \
+lib32-libldap \
+gnutls \
+lib32-gnutls \
+mpg123 \
+lib32-mpg123 \
+openal \
+lib32-openal \
+v4l-utils \
+lib32-v4l-utils \
+libpulse \
+lib32-libpulse \
+alsa-plugins \
+lib32-alsa-plugins \
+alsa-lib \
+lib32-alsa-lib \
+libjpeg-turbo \
+lib32-libjpeg-turbo \
+libxcomposite \
+lib32-libxcomposite \
+libxinerama \
+lib32-libxinerama \
+ncurses \
+lib32-ncurses \
+opencl-icd-loader \
+lib32-opencl-icd-loader \
+libxslt \
+lib32-libxslt \
+gst-plugins-base-libs \
+lib32-gst-plugins-base-libs \
+vkd3d \
+lib32-vkd3d \
+sdl2 \
+lib32-sdl2 \
+libgphoto2 \
+sane \
+gsm \
+cups \
+samba \
+libxmu \
+lib32-libxmu \
+libxxf86vm \
+lib32-libxxf86vm \
+pcre \
+lib32-pcre \
+bzip2 \
+lib32-bzip2 \
+harfbuzz \
+lib32-harfbuzz
+
+# wine
+pacman -Syw --noconfirm --cachedir cache \
+wine
+
+# glibc
+pacman -Syw --noconfirm --cachedir cache \
+glibc \
+lib32-glibc \
+zlib \
+lib32-zlib \
+libstdc++5
+
+# drivers
+pacman -Syw --noconfirm --cachedir cache \
+mesa \
+lib32-mesa \
+mesa-libgl \
+lib32-mesa-libgl \
+ocl-icd \
+lib32-ocl-icd \
+vulkan-icd-loader \
+lib32-vulkan-icd-loader \
+libdrm \
+lib32-libdrm \
+libva \
+lib32-libva \
+vulkan-intel \
+lib32-vulkan-intel \
+vulkan-radeon \
+lib32-vulkan-radeon \
+vulkan-tools
+
+# core deps
+sudo pacman -Syw --noconfirm --cachedir cache \
+coreutils \
+grep \
+bash \
+readline \
+lib32-readline
+
+pip3 install appimage-builder
+
+appimage-builder --skip-tests --recipe wine-stable.yml
+
+ls -al
+
 
 # wineworkdir cleanup
 # rm -rf cache; rm -rf include; rm usr/lib32/{*.a,*.o}; rm -rf usr/lib32/pkgconfig; rm -rf share/man; rm -rf usr/include; rm -rf usr/share/{applications,doc,emacs,gtk-doc,java,licenses,man,info,pkgconfig}; rm usr/lib32/locale
